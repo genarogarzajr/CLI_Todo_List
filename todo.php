384d0d44ddfeb8d21d77ca08bfd51cfacd8cc8d8
Genaro Garza <genarogarza@MyMacbook.local>
2014-05-14 14:47:58 -0500

    version 1

--- todo.php
+++ todo.php
@@ -19,21 +19,22 @@ do {
     $input = trim(fgets(STDIN));
 
     // Check for actionable input
-    if ($input == 'N') {
+    if ($input == 'N' || $input == 'n') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
         $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R') {
+    } elseif ($input == 'R' || $input == 'r') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
         $key = trim(fgets(STDIN));
         // Remove from array
         unset($items[$key]);
-    }
-// Exit when input is (Q)uit
-} while ($input != 'Q');
+    } 
+
+    // Exit when input is (Q)uit
+} while ($input != 'Q' && $input != 'q');
 
 // Say Goodbye!
 echo "Goodbye!\n";
